5472fdc8dc353856b9ebcda0b279308d55606415
eee11223344 <164818913+eee11223344@users.noreply.github.com>
2024-04-01 10:12:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function switchImage() {
+    let image1 = document.getElementById('image1');
+    let image2 = document.getElementById('image2');
+
+    if (image1.style.display !== 'none') {
+        image1.style.display = 'none';
+        image2.style.display = 'block';
+    } else {
+        image1.style.display = 'block';
+        image2.style.display = 'none';
+    }
+}
